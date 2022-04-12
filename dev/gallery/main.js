24b7e2fced5c9b68532e0e4bd093a6d48fcd5d07
Siddharth Gupta <siddharthgupta052@gmail.com>
2022-04-12 23:39:32 +0530

    gallery project

--- dev/gallery/main.js
+++ dev/gallery/main.js
@@ -3,6 +3,11 @@
 let plus=document.querySelector("#plus");
 let imageListContainer=document.querySelector(".img-list-container");
 let imageShowContainer=document.querySelector(".img-show_container");
+let imgsArr=[];
+//dhyan dena padega
+let cimgIndex=-1;
+let uniqueIdentifier=0;
+
 plus.addEventListener("click",function(){
     let imgLink=prompt("ENTER THE IMAGE URL");
     if(imgLink ==null || imgLink==""){
@@ -16,9 +21,18 @@ plus.addEventListener("click",function(){
 //ye function help karega list and container main image dalne main 
 function addImageToListandShow(imgLink){
 
+    //img list
+    let previewImg=document.createElement("img");
+    previewImg.setAttribute("src", imgLink);
+    previewImg.setAttribute("cid", uniqueIdentifier);
+    uniqueIdentifier++;
+    previewImg.setAttribute("class","img_preview");
+    //last main add karta hai
+    imageListContainer.appendChild(previewImg);
 
-//img list ke ley
-let innerHTMLList=`<img class="img_preview" src=${imgLink} alt="">`
+
+//img list ke ley ye bhi kar sakte the final img ki tarah
+// let innerHTMLList=`<img class="img_preview" src=${imgLink} alt="">`
 
 //final image ke ley
 let innerHTMLShow=`<span action class="material-icons">keyboard_double_arrow_left</span>
@@ -28,5 +42,12 @@ let innerHTMLShow=`<span action class="material-icons">keyboard_double_arrow_lef
 <span action="right" class="material-icons">keyboard_double_arrow_right</span>
 `
 imageShowContainer.innerHTML=innerHTMLShow;
-imageListContainer.innerHTML=innerHTMLList; 
+
+let imgIdentifierObj={
+    cid:uniqueIdentifier,
+    url:imgLink
+}
+imgsArr.push(imgIdentifierObj);
+// console.log(imgsArr);
+
 }
