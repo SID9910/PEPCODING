4f1b87fb4585f9ba99d63b5d8af844b9a493524e
Siddharth Gupta <siddharthgupta052@gmail.com>
2022-05-12 09:48:27 +0530

    dsa

--- dev/node module/os.js
+++
@@ -1,8 +0,0 @@
-              //topic:os module(operation system module)inbuilt module
-
-const os=require('os')
-
-console.log(os.arch())//os.arch gives architecture of os
-console.log(os.platform())//os.platform gives platform of os
-console.log(os.networkInterfaces())//ye pura network ke bare main chize bataega
-console.log(os.cpus())//os.cpus se cpu ki info milegi
