82198b1260bfd113a30cd2d00336a72e07de5d8b
Siddharth Gupta <siddharthgupta052@gmail.com>
2022-05-17 14:49:19 +0530

    trees

---
+++ DSA/LEVEL -2/TREES/LEFT VIEW OF BINARY TREE.java
@@ -0,0 +1,40 @@
+
+    class Tree
+    {
+        //queue main jo add karenge uska size nikal lenge and uska first index store kar lenge arraylist 
+        //main and aakhri main return kar denge arraylist ko
+        ArrayList<Integer> leftView(Node root)
+        {
+            ArrayList<Integer> retval = new ArrayList<>();
+         if (root == null){
+             return retval;
+             }
+             Queue<Node> queue = new LinkedList<>();
+             queue.add(root);
+             
+             while(queue.size()>0){
+                 
+                 int lsize =queue.size();
+                 for(int i=0 ;i<lsize ;i++){
+                   
+                     Node temp =queue.remove();
+            
+                     if(i==0){
+                         retval.add(temp.data);
+                         
+                     }
+                     if(temp.left!=null){
+                         queue.add(temp.left);
+                     }
+                     
+                     
+                     if(temp.right!=null){
+                         queue.add(temp.right);
+                     }
+                 } 
+        
+             }
+       
+       return retval;
+       }
+    }
