32ddf73280579c9e2c181673cfbfee283fc7d0d3
Siddharth Gupta <siddharthgupta052@gmail.com>
2022-05-24 12:57:19 +0530

    trees

---
+++ DSA/LEVEL -2/TREES/(222) Count Complete Tree Nodes.java
@@ -0,0 +1,37 @@
+
+class Solution {
+    //     public int countNodes(TreeNode root) {
+    //       if(root ==null){
+    //           return 0;
+    //       }  
+    //         int left =countNodes(root.left);
+    //         int right =countNodes(root.right);
+    //         int ts=left+right+1;
+    //         return ts;
+    //     }
+        public int countNodes(TreeNode root){
+            if(root ==null){
+                return 0 ;
+            }
+            int left =0;
+            TreeNode node=root.left;
+            while(node!= null){
+                node =node.left;
+                left++;
+            }
+            int right =0;
+            node =root.right;
+            while(node!=null){
+              node =node.right;
+                right++;
+            }
+            if(left ==right){
+                int ht=left+1;
+                return (1 << ht)-1;
+            }else{
+                return countNodes(root.left)+countNodes(root.right)+1;
+            }
+            
+        }
+        
+    }
