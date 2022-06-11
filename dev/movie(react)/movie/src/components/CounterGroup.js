a7814a61a0edd45ef749e7aeb90419f11dc31a4a
Siddharth Gupta <siddharthgupta052@gmail.com>
2022-06-11 22:36:31 +0530

    React

---
+++ dev/movie(react)/movie/src/components/CounterGroup.js
@@ -0,0 +1,37 @@
+//   typre rfce as shortcut and extension react E57
+//14th april
+import React from 'react'
+import "./CounterGroup.css"
+import Counter from "./Counter";
+function CounterGroup(props) {
+    let { globalCounterNum, globalCounterVal,resetGlobals } = props;
+    // way 1 
+    // let globalCounterNum = props.globalCounterNum;
+    return (
+        <div>
+            <h2>CounterGroup</h2>
+            <div className="counter-group">
+                {/* 1 */}
+                <Counter number={1}
+                    globalCounterNum={globalCounterNum}
+                    globalCounterVal={globalCounterVal}
+                    resetGlobals={resetGlobals}
+                ></Counter>
+                <Counter number={2}
+                    globalCounterNum={globalCounterNum}
+                    globalCounterVal={globalCounterVal}
+                    resetGlobals={resetGlobals}
+                ></Counter>
+                <Counter number={3}
+                    globalCounterNum={globalCounterNum}
+                    globalCounterVal={globalCounterVal}
+                    resetGlobals={resetGlobals}
+
+                ></Counter>
+            </div>
+        </div>
+
+
+    )
+}
+export default CounterGroup;
