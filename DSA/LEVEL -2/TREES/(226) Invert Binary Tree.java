99de62ca5e5e5d1d6eee345467d6b78d0cb85a00
Siddharth Gupta <siddharthgupta052@gmail.com>
2022-08-02 13:35:14 +0530

    array

---
+++ DSA/LEVEL -2/TREES/(226) Invert Binary Tree.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    //basically bss swap kardo kardo right child ko left child se bss
+    //and left child ko right child se bss 
+    public TreeNode invertTree(TreeNode root) {
+      if(root == null) return null;
+        TreeNode tmp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(tmp);
+        return root;
+    }
+}
