0828967316a98214a41c0b796bc965c58a7efbe7
Siddharth Gupta <siddharthgupta052@gmail.com>
2022-08-03 02:06:22 +0530

    array

--- DSA/LEVEL -2/ARRAY/(238) Product of Array Except Self.java
+++ DSA/LEVEL -2/ARRAY/(238) Product of Array Except Self.java
@@ -1,22 +1,32 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        //left main khud ko chod kar saare left ka multiply store
-        //kar lenge
         int[] left = new int[nums.length];
+
+        //bss left store karo
         for(int i=0;i<nums.length;i++){
             if(i==0){
                 left[i]=1;
                 
             }else{
-                left[i]=left[i-1]*nums[i-1];//isme left store ho jaega
+                left[i]=left[i-1]*nums[i-1];
             }
         }
-        //bss last se right ke store karte jao
-        int right =1;
-        for(int i=nums.length-2;i>=0;i--){
-            right =right*nums[i+1];//bss multiply karte jao
-            left[i] =left[i]*right;
+        //bss right store karo
+        int[] right = new int[nums.length];
+        for(int i=nums.length-1;i>=0;i--){
+            if(i==nums.length-1){
+                right[i]=1;
+                
+            }else{
+                right[i]=nums[i+1]*right[i+1];
+            }
         }
-        return left;
+        //bss aab left and right ko multiply kardo 
+        int[] res = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            res[i]=left[i]*right[i];
+        }
+        
+        return res;
     }
 }
